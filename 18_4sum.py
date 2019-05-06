e54d9eb17447757518a6ade3e04bd4c685f5820a
zlchen <zlchen@tongji.edu.cn>
2019-05-06 14:23:08 +0800

    add 18_4sum

--- 18_4sum.py
+++ 18_4sum.py
@@ -5,21 +5,25 @@ class Solution:
             return []
         nums.sort()
         ans = []
-        end = length - 1
+
         for i in range(length - 3):
             for j in range(i + 1, length - 2):
                 start = j + 1
+                end = length - 1
                 while start < end:
                     temp = nums[i] + nums[j] + nums[start] + nums[end]
-                    if temp == target and [nums[i], nums[j], nums[start], nums[end]] not in ans:
-                        ans.append([nums[i], nums[j], nums[start], nums[end]])
+                    if temp == target:
+                        temp_ans = [nums[i], nums[j], nums[start], nums[end]]
+                        if temp_ans not in ans:
+                            ans.append([nums[i], nums[j], nums[start], nums[end]])
+                        start += 1
+                        end -= 1
                     elif temp < target:
                         start += 1
                     elif temp > target:
                         end -= 1
 
         return ans
-
 
 if __name__ == '__main__':
     nums = [1, 0, -1, 0, -2, 2]
